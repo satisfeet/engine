4cf5ad5b5f66938ddf20c01a866fcc92b204a446
Bodo Kaiser <i@bodokaiser.io>
2014-03-09 18:39:00 +0100

    added customer form view

--- app/routes/customers/index.js
+++ app/routes/customers/index.js
@@ -1,17 +1,27 @@
 var CustomerItem = require('./item');
 var CustomerList = require('./list');
+var CustomerForm = require('./form');
 
 module.exports = function(app) {
 
-  app('/customers', function(context, next) {
+  app('/customers/list', function(context, next) {
     var element = context.element;
-    var collection = context.customers;
+    var customers = context.customers;
 
     while (element.firstElementChild) {
       element.firstElementChild.remove();
     }
+    element.appendChild(new CustomerList(customers).element);
+  });
 
-    element.appendChild(new CustomerList(collection).element);
+  app('/customers/create', function(context, next) {
+    var element = context.element;
+    var customer = context.customer;
+
+    while (element.firstElementChild) {
+      element.firstElementChild.remove();
+    }
+    element.appendChild(new CustomerForm(customer).element);
   });
 
 };
