458b47625508067e9f13fd60a6976cbdb36746eb
Bodo Kaiser <i@bodokaiser.io>
2014-03-14 15:49:30 +0100

    added tests for HTTP /customers

--- usr/tests/customers.js
+++ usr/tests/customers.js
@@ -1,21 +1,56 @@
 var chai      = require('chai');
+var lodash    = require('lodash');
 var supertest = require('supertest');
 
 var app = require('../../lib');
 
+var customer = {
+  surname: 'Kaiser',
+  forename: 'Bodo',
+  email: 'i@bodokaiser.io',
+  street: 'Geiserichstraße',
+  street_nr: '3',
+  city: 'Berlin',
+  zip: 12105
+};
+
 describe('HTTP: customers', function() {
+
+  describe('POST /customers', function() {
+
+    it('should respond json', function(done) {
+      var raw = lodash.omit(customer, 'id');
+
+      supertest(app).post('/customers').accept('json')
+        .send(customer)
+        .expect('Content-Type', /json/)
+        .expect(200, function(err, res) {
+          if (err) throw err;
+
+          chai.expect(res.body).to.be.an('object');
+          chai.expect(res.body.id).to.be.a('number');
+
+          customer.id = res.body.id;
+          customer.company = null;
+
+          chai.expect(customer).to.eql(res.body);
+
+          done();
+        });
+    });
+
+  });
 
   describe('GET /customers', function() {
 
     it('should respond json', function(done) {
       supertest(app).get('/customers').accept('json')
         .expect('Content-Type', /json/)
-        .expect(200, done);
+        .expect(200, [customer], done);
     });
 
-  });
-
-  describe('POST /customers', function() {
+    // TODO: full-text search
+    // TODO: filter by parameter
 
   });
 
@@ -28,6 +63,11 @@ describe('HTTP: customers', function() {
   });
 
   describe('DELETE /customers/:id', function() {
+
+    it('should respond OK', function(done) {
+      supertest(app).del('/customers/' + customer.id).accept('json')
+        .expect(200, done);
+    });
 
   });
 
