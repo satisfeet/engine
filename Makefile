937e2589fb31876ef2675897351d3c2d6365efae
Bodo Kaiser <i@bodokaiser.io>
2014-03-25 17:30:03 +0100

    added http basic, updated tests

--- Makefile
+++ Makefile
@@ -12,6 +12,6 @@ boot:
 	$(SUPERVISOR) $(SUPERVISOR_FLAGS) lib
 
 test:
-	$(MOCHA) $(MOCHA_FLAGS) test
+	$(MOCHA) $(MOCHA_FLAGS) opt/test
 
 .PHONY: test
--- opt/test/customers.js
+++ opt/test/customers.js
@@ -2,37 +2,28 @@ var chai      = require('chai');
 var lodash    = require('lodash');
 var supertest = require('supertest');
 
-var app = require('../lib');
-
-var customer = {
-  name: 'Willy Smith',
-  email: 'willy@example.io',
-  address: {
-    street: 'Geiserichstraße 3',
-    city: 'Berlin',
-    zip: 12105
-  }
-};
+var app   = require('../../lib');
+var auth  = app.settings.account;
+var model = require('../data/customer');
 
 describe('HTTP: customers', function() {
 
   describe('POST /customers', function() {
 
     it('should respond json', function(done) {
-      var raw = lodash.omit(customer, 'id');
-
-      supertest(app).post('/customers').accept('json')
-        .send(customer)
+      supertest(app).post('/customers')
+				.send(model)
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect('Content-Type', /json/)
         .expect(200, function(err, res) {
           if (err) throw err;
 
+          model.id = res.body.id;
+
           chai.expect(res.body).to.be.an('object');
           chai.expect(res.body.id).to.be.a('string');
-
-          customer.id = res.body.id;
-
-          chai.expect(customer).to.eql(res.body);
+          chai.expect(model).to.eql(res.body);
 
           done();
         });
@@ -43,30 +34,38 @@ describe('HTTP: customers', function() {
   describe('GET /customers', function() {
 
     it('should respond json', function(done) {
-      supertest(app).get('/customers').accept('json')
+      supertest(app).get('/customers')
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect('Content-Type', /json/)
-        .expect(200, [customer], done);
+        .expect(200, [model], done);
     });
 
     it('should respond json filtered by email', function(done) {
-      supertest(app).get('/customers').accept('json')
+      supertest(app).get('/customers')
+				.auth(auth.username, auth.password)
         .query({ filter: { email: '.io' } })
+				.accept('application/json')
         .expect('Content-Type', /json/)
-        .expect(200, [customer], done);
+        .expect(200, [model], done);
     });
 
     it('should respond json filtered by street', function(done) {
-      supertest(app).get('/customers').accept('json')
+			supertest(app).get('/customers')
+				.auth(auth.username, auth.password)
         .query({ filter: { address: { street: '.io' } } })
+				.accept('application/json')
         .expect('Content-Type', /json/)
         .expect(200, [], done);
     });
 
     it('should respond json searched by query', function(done) {
-      supertest(app).get('/customers').accept('json')
+      supertest(app).get('/customers')
+				.auth(auth.username, auth.password)
         .query({ search: 'Berl' })
+				.accept('application/json')
         .expect('Content-Type', /json/)
-        .expect(200, [customer], done);
+        .expect(200, [model], done);
     });
 
   });
@@ -74,13 +73,17 @@ describe('HTTP: customers', function() {
   describe('GET /customers/:id', function() {
 
     it('should respond json', function(done) {
-      supertest(app).get('/customers/' + customer.id).accept('json')
+      supertest(app).get('/customers/' + model.id)
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect('Content-Type', /json/)
-        .expect(200, customer, done);
+        .expect(200, model, done);
     });
 
     it('should respond not found', function(done) {
-      supertest(app).get('/customers/abc').accept('json')
+      supertest(app).get('/customers/abc')
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect('Content-Type', /json/)
         .expect(404, done);
     });
@@ -90,16 +93,20 @@ describe('HTTP: customers', function() {
   describe('PUT /customers/:id', function() {
 
     it('should respond json', function(done) {
-      customer.street = 'Geiserichstr.';
+      model.street = 'Geiserichstr.';
 
-      supertest(app).put('/customers/' + customer.id).accept('json')
-        .send(customer)
+      supertest(app).put('/customers/' + model.id)
+        .send(model)
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
 
     it('should respond not found', function(done) {
-      supertest(app).put('/customers/abc').accept('json')
+      supertest(app).put('/customers/abc')
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect('Content-Type', /json/)
         .expect(404, done);
     });
@@ -109,7 +116,9 @@ describe('HTTP: customers', function() {
   describe('DELETE /customers/:id', function() {
 
     it('should respond OK', function(done) {
-      supertest(app).del('/customers/' + customer.id).accept('json')
+      supertest(app).del('/customers/' + model.id)
+				.auth(auth.username, auth.password)
+				.accept('application/json')
         .expect(200, done);
     });
 
