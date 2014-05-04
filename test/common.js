478b3e43cd6501002bd6a2c92a9692fdf69af2de
Bodo Kaiser <i@bodokaiser.io>
2014-05-04 10:24:00 +0200

    implemented /session route for jwt request

--- test/common.js
+++ test/common.js
@@ -1,3 +1,4 @@
+var chai      = require('chai');
 var supertest = require('supertest');
 
 var hooks = require('./hooks');
@@ -37,6 +38,56 @@ describe('GET /', function() {
       .expect('Access-Control-Allow-Methods', METHODS)
       .expect('Access-Control-Allow-Credentials', CREDENTIALS)
       .expect(406, done);
+  });
+
+});
+
+describe('POST /session', function() {
+
+  it('should respond "OK"', function(done) {
+    supertest(this.app).post('/session')
+      .send({
+        username: this.username,
+        password: this.password
+      })
+      .expect(200, function(err, res) {
+        if (err) return done(err);
+
+        chai.expect(res.body).to.be.an('object')
+          .and.to.have.property('token').that.is.a('string');
+
+        done();
+      });
+  });
+
+  it('should respond "Unauthorized"', function(done) {
+    supertest(this.app).post('/session')
+      .expect(401, done);
+  });
+
+  it('should respond "Unauthorized"', function(done) {
+    supertest(this.app).post('/session')
+      .send({
+        username: this.username
+      })
+      .expect(401, done);
+  });
+
+  it('should respond "Unauthorized"', function(done) {
+    supertest(this.app).post('/session')
+      .send({
+        password: this.password
+      })
+      .expect(401, done);
+  });
+
+});
+
+describe('DELETE /session', function() {
+
+  it('should respond "Method Not Allowed"', function(done) {
+    supertest(this.app).del('/session')
+      .expect(405, done);
   });
 
 });
