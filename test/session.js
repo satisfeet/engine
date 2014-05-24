973b1c2a010f8584cf0014762cbb19afb293ea90
Bodo Kaiser <i@bodokaiser.io>
2014-05-24 08:48:54 +0200

    merged test/common with test/session, moved hooks to dir

--- test/session.js
+++ test/session.js
@@ -1,9 +1,66 @@
 var chai      = require('chai');
 var supertest = require('supertest');
 
+const ORIGIN  = 'http://manager.satisfeet.me';
+const HEADERS = 'Accept, Authorization, Content-Type';
+const METHODS = 'GET, HEAD, PUT, POST, DELETE, OPTIONS';
+
 var hooks = require('./hooks');
 
 before(hooks.setup);
+
+describe('GET /', function() {
+
+  before(hooks.customers.create);
+  before(hooks.products.create);
+  before(hooks.orders.create);
+
+  it('should respond "OK"', function(done) {
+    supertest(this.app).get('/')
+      .set('Authorization', this.token)
+      .expect('Access-Control-Allow-Origin', ORIGIN)
+      .expect('Access-Control-Allow-Headers', HEADERS)
+      .expect('Access-Control-Allow-Methods', METHODS)
+      .expect({
+        orders: 1,
+        products: 1,
+        customers: 1
+      })
+      .expect(200, done);
+  });
+
+  it('should respond "Unauthorized"', function(done) {
+    supertest(this.app).get('/')
+      .expect('Access-Control-Allow-Origin', ORIGIN)
+      .expect('Access-Control-Allow-Headers', HEADERS)
+      .expect('Access-Control-Allow-Methods', METHODS)
+      .expect(401, done);
+  });
+
+  it('sshould respond "Not Acceptable"', function(done) {
+    supertest(this.app).get('/')
+      .set('Authorization', this.token)
+      .accept('xml')
+      .expect('Access-Control-Allow-Origin', ORIGIN)
+      .expect('Access-Control-Allow-Headers', HEADERS)
+      .expect('Access-Control-Allow-Methods', METHODS)
+      .expect(406, done);
+  });
+
+  after(hooks.orders.remove);
+  after(hooks.products.remove);
+  after(hooks.customers.remove);
+
+});
+
+describe('OPTIONS /', function() {
+
+  it('should respond "No Content"', function(done) {
+    supertest(this.app).options('/')
+      .expect(204, done);
+  });
+
+});
 
 describe('GET /session', function() {
 
