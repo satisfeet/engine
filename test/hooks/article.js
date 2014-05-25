3025d86e28359e58c592eed1a6bc1b7cc5232df1
Bodo Kaiser <i@bodokaiser.io>
2014-05-25 16:32:05 +0200

    updated model structure
    - renamed Article to Product
    - renamed Variety to Article
    - Article only has details hash
    - Product keeps track of articles

--- test/hooks/article.js
+++ test/hooks/article.js
@@ -4,20 +4,11 @@ var Article = mongoose.models.Article;
 
 exports.create = function(done) {
   this.article = new Article({
-    title: 'Casual Socks',
-    types: [
-      'clothing'
-    ],
     details: {
-      material: {
-        cotton: 0.87
-      }
-    },
-    pricing: {
-      retail: 2.99
+      color: 'red',
+      size: 44
     }
   });
-
   this.article.save(done);
   this.article = this.article.toJSON();
 };
