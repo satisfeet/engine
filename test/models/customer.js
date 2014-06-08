8e2527e04eda65f8da50a63231a4d2043027f88c
Bodo Kaiser <i@bodokaiser.io>
2014-06-08 10:21:33 +0200

    normalized model tests

--- test/models/customer.js
+++ test/models/customer.js
@@ -7,25 +7,7 @@ before(hooks.setup);
 
 describe('Customer', function() {
 
-  describe('#get', function() {
-
-    it('should return image', function() {
-      var customer = new this.Customer({ email: 'i@bodokaiser.io' });
-
-      chai.expect(customer)
-        .to.have.property('image')
-        .to.be.an('object')
-        .to.have.property('url')
-        .to.be.a('string');
-
-      chai.expect(url.parse(customer.image.url))
-        .to.have.property('host')
-        .to.equal('www.gravatar.com');
-    });
-
-  });
-
-  describe('#set', function() {
+  describe('#name', function() {
 
     it('should sanitize name', function() {
       var customer = new this.Customer();
@@ -37,6 +19,36 @@ describe('Customer', function() {
         .to.equal('Bodo Kaiser');
     });
 
+    it('should validate required', function(done) {
+      var customer = new this.Customer();
+
+      customer.validate(function(err) {
+        chai.expect(err)
+          .to.have.property('errors')
+          .to.have.property('name')
+          .to.have.property('type')
+          .to.equal('required');
+
+        done();
+      });
+    });
+
+    it('should not pass name error', function(done) {
+      var customer = new this.Customer({ name: 'Hans Müller' });
+
+      customer.validate(function(err) {
+        chai.expect(err)
+          .to.have.property('errors')
+          .to.not.have.property('name');
+
+        done();
+      });
+    });
+
+  });
+
+  describe('#email', function() {
+
     it('should sanitize email', function() {
       var customer = new this.Customer();
 
@@ -47,47 +59,11 @@ describe('Customer', function() {
         .to.equal('i@bodokaiser.io');
     });
 
-    it('should sanitize company', function() {
-      var customer = new this.Customer();
-
-      customer.company = '  Satisfeet GmbH';
-
-      chai.expect(customer)
-        .to.have.property('company')
-        .to.equal('Satisfeet GmbH');
-    });
-
-    it('should sanitize address', function() {
-      var customer = new this.Customer();
-
-      customer.address.street = ' ForstER sTraße 3';
-      customer.address.city = ' BAD LAusitz ';
-
-      chai.expect(customer)
-        .to.have.property('address')
-        .to.have.property('street')
-        .to.equal('Forster Straße 3');
-      chai.expect(customer)
-        .to.have.property('address')
-        .to.have.property('city')
-        .to.equal('Bad Lausitz');
-    });
-
-  });
-
-  describe('#validate', function() {
-
-    it('should return required errors', function(done) {
+    it('should validate required', function(done) {
       var customer = new this.Customer();
 
       customer.validate(function(err) {
         chai.expect(err).to.exist;
-
-        chai.expect(err)
-          .to.have.property('errors')
-          .to.have.property('name')
-          .to.have.property('type')
-          .to.equal('required');
 
         chai.expect(err)
           .to.have.property('errors')
@@ -105,19 +81,7 @@ describe('Customer', function() {
       });
     });
 
-    it('should not return name error', function(done) {
-      var customer = new this.Customer({ name: 'Hans Müller' });
-
-      customer.validate(function(err) {
-        chai.expect(err)
-          .to.have.property('errors')
-          .to.not.have.property('name');
-
-        done();
-      });
-    });
-
-    it('should not return email error', function(done) {
+    it('should not pass email error', function(done) {
       var customer = new this.Customer({ email: 'i@bodokaiser.io' });
 
       customer.validate(function(err) {
@@ -129,7 +93,39 @@ describe('Customer', function() {
       });
     });
 
-    it('should not return company error', function(done) {
+  });
+
+  describe('#image', function() {
+
+    it('should build image of email', function() {
+      var customer = new this.Customer({ email: 'i@bodokaiser.io' });
+
+      chai.expect(customer)
+        .to.have.property('image')
+        .to.be.an('object')
+        .to.have.property('url')
+        .to.be.a('string');
+
+      chai.expect(url.parse(customer.image.url))
+        .to.have.property('host')
+        .to.equal('www.gravatar.com');
+    });
+
+  });
+
+  describe('#company', function() {
+
+    it('should sanitize company', function() {
+      var customer = new this.Customer();
+
+      customer.company = '  Satisfeet GmbH';
+
+      chai.expect(customer)
+        .to.have.property('company')
+        .to.equal('Satisfeet GmbH');
+    });
+
+    it('should not pass company error', function(done) {
       var customer = new this.Customer({ email: 'BlaBla Ltd.' });
 
       customer.validate(function(err) {
@@ -141,7 +137,41 @@ describe('Customer', function() {
       });
     });
 
-    it('should not return address error', function(done) {
+  });
+
+  describe('#address', function() {
+
+    it('should sanitize address', function() {
+      var customer = new this.Customer();
+
+      customer.address.street = ' ForstER sTraße 3';
+      customer.address.city = ' BAD LAusitz ';
+
+      chai.expect(customer)
+        .to.have.property('address')
+        .to.have.property('street')
+        .to.equal('Forster Straße 3');
+      chai.expect(customer)
+        .to.have.property('address')
+        .to.have.property('city')
+        .to.equal('Bad Lausitz');
+    });
+
+    it('should validate required', function(done) {
+      var customer = new this.Customer();
+
+      customer.validate(function(err) {
+        chai.expect(err)
+          .to.have.property('errors')
+          .to.have.property('address.city')
+          .to.have.property('type')
+          .to.equal('required');
+
+        done();
+      });
+    });
+
+    it('should not pass address error', function(done) {
       var customer = new this.Customer({
         address: {
           street: '123 Street Avenue',
